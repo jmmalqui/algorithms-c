ccd4cb0cacfb11357080ff112bf704571d7b5959
johanmallqui <johanmmalqui@gmail.com>
2023-11-21 22:09:01 +0900

    commit

--- sorts/quicksort.c
+++ sorts/quicksort.c
@@ -2,7 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 #define ARRAY_SIZE(N) ((sizeof N) / (sizeof N[0]))
-
+void debug_array(int array[], int size)
+{
+    printf("[");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+        if (i != size - 1)
+        {
+            printf(",");
+        }
+    }
+    printf("]\n");
+}
 int quicksort_recursive(int array[], int array_size, int begin, int end)
 {
     if (end <= begin)
@@ -15,7 +27,6 @@ int quicksort_recursive(int array[], int array_size, int begin, int end)
     int temp = 0;
     while (true)
     {
-
         if (j == end)
         {
             i++;
@@ -23,21 +34,8 @@ int quicksort_recursive(int array[], int array_size, int begin, int end)
             array[i] = array[pivot_d];
             array[pivot_d] = temp;
             pivot_d = i;
-            printf("i: %d, j: %d, begin: %d, end: %d {", i, j, begin, end);
-            for (int i = 0; i < array_size; i++)
-            {
-                printf("%d ", array[i]);
-            }
-            printf("}\n");
             break;
         }
-
-        printf("i: %d, j: %d, begin: %d, end: %d, pivot: %d {", i, j, begin, end, pivot_d);
-        for (int i = 0; i < array_size; i++)
-        {
-            printf("%d ", array[i]);
-        }
-        printf("}\n");
         if (array[j] < array[pivot_d])
         {
             i++;
@@ -53,7 +51,6 @@ int quicksort_recursive(int array[], int array_size, int begin, int end)
             continue;
         }
     }
-    printf("\n\n");
     quicksort_recursive(array, array_size, begin, pivot_d - 1);
     quicksort_recursive(array, array_size, pivot_d + 1, end);
     return 0;
@@ -67,9 +64,11 @@ int quicksort_not_recursive(int array[])
 
 int main(int argc, char const *argv[])
 {
-    int myarray[] = {1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    int myarray[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     int array_size = ARRAY_SIZE(myarray);
+    debug_array(myarray, array_size);
     quicksort_not_recursive(myarray);
     quicksort_recursive(myarray, array_size, 0, array_size - 1);
+    debug_array(myarray, array_size);
     return 0;
 }
